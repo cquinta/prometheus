2a4a06b3d4af7f313715422861a660f27aafdd7a
Carlos Quintanilha <cquinta@gmail.com>
2023-06-22 20:25:27 -0300

    ajuste em arquivo de configuração

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 mkdir /etc/prometheus
 mkdir /var/lib/prometheus
 mkdir /var/log/prometheus
-#addgroup --system prometheus
+addgroup prometheus
 adduser --shell /sbin/nologin --system --ingroup prometheus prometheus
 curl -LO https://github.com/prometheus/prometheus/releases/download/v2.38.0/prometheus-2.38.0.linux-amd64.tar.gz
 tar -xvf prometheus-2.38.0.linux-amd64.tar.gz
@@ -23,30 +23,30 @@ scrape_configs:
       - targets: ["localhost:9090"]
 EOF
 cat <<EOF >> /etc/systemd/system/prometheus.service
-[Unit] # Inicio da definição do serviço.
-Description=Prometheus # Descrição do serviço.
-Documentation=https://prometheus.io/docs/introduction/overview/ # Documentação do serviço.
-Wants=network-online.target # Para que o serviço do Prometheus seja iniciado, precisamos antes que o serviço de rede esteja ativo.
-After=network-online.target # Depois que o serviço de rede esteja ativo, o serviço do Prometheus será iniciado.
+[Unit]
+Description=Prometheus
+Documentation=https://prometheus.io/docs/introduction/overview/
+Wants=network-online.target
+After=network-online.target
 
-[Service] # Inicio da definição do serviço.
-Type=simple # Tipo do serviço, o padrão é simple, ou seja, o serviço é simples, não tem subserviços.
-User=prometheus # Usuário do serviço, o padrão é prometheus, o mesmo que criamos no passo anterior.
-Group=prometheus # Grupo do serviço, o padrão é prometheus, o mesmo que criamos no passo anterior.
-ExecReload=/bin/kill -HUP \$MAINPID # Comando para o serviço do Prometheus ser reiniciado, o padrão é /bin/kill -HUP \$MAINPID, ou seja, o serviço do Prometheus será reiniciado ao receber um sinal de reinicialização.
-ExecStart=/usr/local/bin/prometheus \ # Comando para o serviço do Prometheus ser iniciado, o padrão é /usr/local/bin/prometheus, o mesmo lugar para onde mandamos o binário do Prometheus no passo anterior.
-  --config.file=/etc/prometheus/prometheus.yml \ # Arquivo de configuração do serviço do Prometheus, o padrão é /etc/prometheus/prometheus.yml, o mesmo lugar onde mandamos o arquivo de configuração do Prometheus no passo anterior.
-  --storage.tsdb.path=/var/lib/prometheus \ # Diretório onde o serviço do Prometheus vai armazenar seus dados, o padrão é /var/lib/prometheus, o mesmo lugar onde criamos o diretório para armazenar os dados do Prometheus.
-  --web.console.templates=/etc/prometheus/consoles \ # Diretório onde o serviço do Prometheus vai encontrar os templates para os consoles, o padrão é /etc/prometheus/consoles, o mesmo lugar para onde movemos o diretório para armazenar os templates dos consoles.
-  --web.console.libraries=/etc/prometheus/console_libraries \ # Diretório onde o serviço do Prometheus vai encontrar as bibliotecas para os consoles, o padrão é /etc/prometheus/console_libraries, o mesmo lugar para onde movemos o diretório para armazenar as bibliotecas dos consoles.
-  --web.listen-address=0.0.0.0:9090 \ # Endereço do serviço do Prometheus, o padrão é o serviço escutar na porta 9090.
-  --web.external-url= # Endereço externo do serviço do Prometheus, por exemplo o endereço DNS do serviço.
+[Service]
+Type=simple
+User=prometheus
+Group=prometheus
+ExecReload=/bin/kill -HUP \$MAINPID
+ExecStart=/usr/local/bin/prometheus \
+  --config.file=/etc/prometheus/prometheus.yml \
+  --storage.tsdb.path=/var/lib/prometheus \
+  --web.console.templates=/etc/prometheus/consoles \
+  --web.console.libraries=/etc/prometheus/console_libraries \
+  --web.listen-address=0.0.0.0:9090 \
+  --web.external-url=
 
-SyslogIdentifier=prometheus # Identificador do serviço no syslog, o padrão é prometheus.
-Restart=always # Reinicialização do serviço, o padrão é always, ou seja, o serviço será reiniciado sempre que ocorrer alguma alteração.
+SyslogIdentifier=prometheus
+Restart=always
 
-[Install] # Inicio da definição do instalador do serviço.
-WantedBy=multi-user.target # Definir em qual grupo o serviço será iniciado, o padrão é multi-user.target.
+[Install]
+WantedBy=multi-user.target
 EOF
 chown -R prometheus:prometheus /etc/prometheus
 chown -R prometheus:prometheus /var/lib/prometheus
